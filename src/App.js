ad9bbb224b3bb77a8fb58bbe177cafbf2c632b14
Mahendran <mahendiranp@users.noreply.github.com>
2024-06-21 10:21:34 +0530

    testcase fixes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello new changexxss</div>;
+  return <div className="App">Hello new changes</div>;
 }
 
 export default App;
